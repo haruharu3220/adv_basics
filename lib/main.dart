f58ef44d495eefd7ef2a8b486641fe5d84541a8d
Hayato <hayato.314.nico@gmail.com>
2023-06-13 07:04:16 +0900

    main.dartの修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('You have pressed the button'),
+        ),
+      ),
+    ),
+  );
+}
