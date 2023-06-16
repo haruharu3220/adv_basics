a072873c94ae741ae7fbc979e2e4852019c8d6a4
Hayato <hayato.314.nico@gmail.com>
2023-06-16 22:56:14 +0900

    講義62 三項演算子による画面の出しわけ

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,29 +1,23 @@
-import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
+
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-// プライベートクラス
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -39,10 +33,12 @@ class _QuizState extends State<Quiz> {
                 Color.fromARGB(255, 107, 15, 168),
               ],
               begin: Alignment.topLeft,
-              end: Alignment.topRight,
+              end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
